14988fed177e289e019e4cd94d20a451fa9adcd4
Dike Ugochukwu <48444706+Henrydykee@users.noreply.github.com>
2021-01-09 12:31:22 +0100

    Update key.dart

--- lib/key.dart
+++ lib/key.dart
@@ -1,6 +1,6 @@
 
 
 class ClientKey{
-  String clientId = 'AdIiy8-53FedoAzGmglDxJciAKj-2-FknBRMhGIun9DA_2s8KD1qXG41Hp7jzjA-dp8ybMjKyAm7ji0W';
-  String secret = 'EDkX3B3QvmV5jhH14YtjUquhDwtMHAN88DMljZmJBemGtCsObiA7yZijsO86HQwOvFAsdicgRpm4XYrL';
+  String clientId = 'YOUR_CLIENT_ID';
+  String secret = 'YOUR_SECRET_KEY';
 }
